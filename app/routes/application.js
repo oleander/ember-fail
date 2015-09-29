dfb7f2a8c9a98c08c6c44dee5bbbe7d1afced207
Linus Oleander <linus@oleander.nu>
2015-09-29 23:58:09 +0200

    Add failing ember 1.x example

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,23 +1,35 @@
 export default Ember.Route.extend({
   afterModel: function() {
+    // Clears db to ensure a clean canvas
+    localStorage.clear();
+
+    var id1 = 1;
+    var id2 = 2;
+    // Populate 
+
     var self = this;
-    var ids = [];
-    for (var i = 40; i > 0; i--) { ids.push(i); }
 
     // Run 2 times
-    for (var n = 2; n > 0; n--) {
-      ids.forEach(function(id) {
-        self.store.findRecord("user", id).then(function(user) {
-          console.info("User already exists with id ", user.get("id"));
-        }).catch(function() {
-          var user = self.store.createRecord("user", {
-            name: "Joe",
-            id: id
-          });
-
-          console.info("User was created with id", user.id);
-        })
+    self.store.find("user", id1).then(function(user) {
+      console.info("User already exists with id ", user.get("id"));
+    }, function() {
+      var user = self.store.createRecord("user", {
+        name: "Joe",
+        id: id1
       });
-    }
+      user.save();
+      console.info("User was created with id", user.id);
+    })
+
+    self.store.findRecord("user", id2).then(function(user) {
+      console.info("User already exists with id ", user.get("id"));
+    }, function() {
+      var user = self.store.createRecord("user", {
+        name: "Joe",
+        id: id2
+      });
+      user.save();
+      console.info("User was created with id", user.id);
+    })
   }
 });
